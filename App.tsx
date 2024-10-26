169d6da7f743a6aa58c3e2c6549096241a4f118c
Ivan <0675361905vi@gmail.com>
2024-10-26 16:35:58 +0300

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+// import { useEffect } from "react";
+import { ActivityIndicator } from "react-native";
+import { useFonts } from "expo-font";
+
+// import LoginScreen from "./screens/LoginScreen";
+import RegistrationScreen from "./screens/RegistrationScreen";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto-Regular.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto-Bold.ttf"),
+    "Roboto-Light": require("./assets/fonts/Roboto-Light.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto-Medium.ttf"),
+  });
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator size="large" />; // Показуй індикатор завантаження
+  }
+
+  // return <LoginScreen />;
+  return <RegistrationScreen />;
+}
